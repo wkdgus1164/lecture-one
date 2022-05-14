f760b4d8165ac8eb734c560bfb9a4d9df6d53461
wkdgus1164 <wkdgus1164@naver.com>
2022-05-15 02:23:37 +0900

    feat: 리팩토링

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,25 +1,36 @@
 import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material'
 import React from 'react'
 import { ListItemModel } from '../pages/Home'
+import styled from '@emotion/styled'
+
+const StyledText = styled(Typography)`
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+`
 
 const ListItem = (props: ListItemModel) =>
-  <Card sx={{ maxWidth: 345 }}>
-    <CardMedia
-      component='img'
-      image={props.thumbnail}
-      alt={props.title}
-    />
-    <CardContent>
-      <Typography gutterBottom variant='h5' component='div'>
-        {props.title}
-      </Typography>
-      <Typography variant='body2' color='text.secondary'>
-        {props.caption}
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size='small'>자세히 보기</Button>
-    </CardActions>
-  </Card>
+	<Card sx={{ maxWidth: 345 }}>
+		<CardMedia
+			component="img"
+			image={props.thumbnail}
+			alt={props.title}
+		/>
+		<CardContent>
+			<StyledText
+				gutterBottom
+				variant="h5">
+				{props.title}
+			</StyledText>
+			<StyledText
+				variant="body2"
+				color="text.secondary">
+				{props.caption}
+			</StyledText>
+		</CardContent>
+		<CardActions>
+			<Button size="small">자세히 보기</Button>
+		</CardActions>
+	</Card>
 
 export default ListItem
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,40 +4,42 @@ import { Grid } from '@mui/material'
 import ListItem from '../components/ListItem'
 
 export type ListItemModel = {
-  thumbnail: string,
-  title: string,
-  caption: string,
+	thumbnail: string,
+	title: string,
+	caption: string,
 }
 
 const Home = () => {
-  const [data, setData] = useState<ListItemModel[]>([])
-
-  const callApi = async () => {
-    axios.get('http://localhost:3001/lecture').then((res) => {
-      setData(res.data)
-    })
-  }
-
-  useEffect(() => {
-    callApi()
-  })
-
-  return <>
-    <Grid container spacing={4} padding='2rem'>
-      {data.map(item =>
-        <Grid item
-              key={item.title}
-              xs={2}
-        >
-          <ListItem key={item.title}
-                    thumbnail={item.thumbnail}
-                    title={item.title}
-                    caption={item.caption}
-          />
-        </Grid>,
-      )}
-    </Grid>
-  </>
+	const [data, setData] = useState<ListItemModel[]>([])
+	
+	const callApi = async () => {
+		axios.get('http://localhost:3001/lecture').then((res) => {
+			setData(res.data)
+		})
+	}
+	
+	useEffect(() => {
+		callApi()
+	})
+	
+	return <>
+		<Grid container spacing={4} padding="2rem">
+			{data.map(item =>
+				<Grid
+					item
+					key={item.title}
+					xs={2}
+				>
+					<ListItem
+						key={item.title}
+						thumbnail={item.thumbnail}
+						title={item.title}
+						caption={item.caption}
+					/>
+				</Grid>,
+			)}
+		</Grid>
+	</>
 }
 
 export default Home
